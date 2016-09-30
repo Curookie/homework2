86c540d50e2c5feff9bef9d0d5c3b9149b42eea6
Curookie <curookie@naver.com>
2016-09-30 14:22:16 +0900

    A06.java 코드 깔끔하게 수정

--- A06.java
+++ A06.java
@@ -4,14 +4,12 @@ public class A06 {
 
 	public static void main(String[] args) {
 
-		A06 a = new A06();
-		a.start();
+		new A06().start();
 	}
 
 	private void start() {
 		
 		Scanner s = new Scanner(System.in);
-		Scanner s2 = new Scanner(System.in);
 
 		System.out.print("이름 : ");
 		String name =s.nextLine();
@@ -26,7 +24,6 @@ public class A06 {
 		System.out.println("["+name+"]"+"님은");
 		System.out.println("평균은 "+avg+"이고,");
 		check(avg);
-
 	}
 
 	private void check(double a) {
@@ -34,30 +31,21 @@ public class A06 {
 		
 		if(a>=95.0) {
 			sco="A+";
-			System.out.println("학점은 "+sco+"입니다.");
 		} else if (a>=90.0) {
 			sco="A";
-			System.out.println("학점은 "+sco+"입니다.");
 		} else if (a>=85.0) {
 			sco="B+";
-			System.out.println("학점은 "+sco+"입니다.");
 		} else if (a>=80.0) {
 			sco="B";
-			System.out.println("학점은 "+sco+"입니다.");
 		} else if (a>=75.0) {
 			sco="C+";
-			System.out.println("학점은 "+sco+"입니다.");
 		} else if (a>=70.0) {
 			sco="C";
-			System.out.println("학점은 "+sco+"입니다.");
 		} else if (a>=65.0) {
 			sco="D+";
-			System.out.println("학점은 "+sco+"입니다.");
 		} else if (a>=60.0) {
-			sco="D+";
-			System.out.println("학점은 "+sco+"입니다.");
-		} else {
-			System.out.println("학점은 "+sco+"입니다.");
+			sco="D";
 		}
+			System.out.println("학점은 "+sco+"입니다.");
 	}
 }
